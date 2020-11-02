b1ceb11d23b9e80aa7f6e31781968317712d095c
Glitch (mlhlocalpython-intro-i) <none>
2020-11-02 22:46:10 +0000

    🎀🐌 Updated with Glitch

---
+++ functions.py
@@ -0,0 +1,13 @@
+your_name = "Jamie"
+
+# Lines 4-5 are a function called 'say_hello'
+def say_hello(name):
+    print("Hey there, {}".format(name))
+  
+say_hello(your_name)
+say_hello("Sam")
+
+def multiply_numbers(num_one, num_2)
+  print(x*y)
+
+multiply_two_numbers(10,5)
---
+++ lists.py
@@ -0,0 +1,12 @@
+# Creates an empty list
+names = []
+
+# Creates a list with values
+planets = ["Mercury", "Venus", "Earth", "Mars"]
+planets.append("Jupiter")
+planets.append("Saturn")
+
+# print(planets)
+# print(planets[0])
+for planet in planets:
+  print(planet)
